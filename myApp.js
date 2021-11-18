78d442d1f0697eb2dc87943fb78d66b213647035
tom perkins <t.perk@live.com.au>
2021-11-18 15:31:06 +1100

    middlewear logging function

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+require('dotenv').config();
+
 console.log("Hello World");
 
 // Task 1 sending html as response
@@ -8,6 +10,13 @@ console.log("Hello World");
 
 //     res.send("Hello Express");
 // });
+
+//task 6 the middleware funciton is called then passes along to the next route for all "/*" path requests
+app.use('/*', function (req, res, next){
+    var myString = req.method + " " + req.path + " - " + req.ip;
+    console.log(myString);
+    next();
+});
 
 //Task 2 sending files
 app.get('/', (req, res) => {
@@ -20,10 +29,25 @@ app.use("/public", express.static(__dirname + "/public"));
 
 
 app.get("/json", (req, res) => {
-    res.json(
-        {messege: "Hello json"}
-    );
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        res.json(
+            {messege: "HELLO JSON"}
+        );
+    }else
+    {
+        res.json(
+            {messege: "Hello json"}
+        );
+    }
 });
+
+
+
+
+// function tomsLogger(req, res, next){
+//     console.log("Im middle ware");
+//     next();
+// }
 
 
 
