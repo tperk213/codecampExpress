0505f156aaa37ad1940f0d5a9ac3b20cb735126a
tom perkins <t.perk@live.com.au>
2021-11-18 12:37:50 +1100

    task 3 send response file to get request on /

--- myApp.js
+++ myApp.js
@@ -3,11 +3,17 @@ var app = express();
 
 console.log("Hello World");
 
+// Task 1 sending html as response
+// app.get('/', (req, res) => {
+
+//     res.send("Hello Express");
+// });
+
+//Task 2 sending files
 app.get('/', (req, res) => {
 
-    res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
 });
-
 
 
 
