84becf33a7881d4f2dc79888f6ce04dfa5a7d46b
tom perkins <t.perk@live.com.au>
2021-11-19 14:53:15 +1100

    added /name form for post

--- myApp.js
+++ myApp.js
@@ -69,6 +69,7 @@ app.get("/:word/echo", (req, res) =>{
 });
 
 
+//task 9ish
 app.get("/name", (req, res)=>{
     res.json({
         name: req.query.first + ' ' + req.query.last
@@ -76,6 +77,11 @@ app.get("/name", (req, res)=>{
 });
 
 
+app.post("/name", (req, res)=>{
+    res.json({
+        name: req.body.first + ' ' + req.body.last
+    });
+});
 
 
 
